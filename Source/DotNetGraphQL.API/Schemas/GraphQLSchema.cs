ea5b5db6fd6a449d8374dfd9d03d4b2c34329ace
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-10-30 16:33:38 -0700

    Use Singleton for ImagesQuery

--- Source/DotNetGraphQL.API/Schemas/GraphQLSchema.cs
+++ Source/DotNetGraphQL.API/Schemas/GraphQLSchema.cs
@@ -1,12 +1,15 @@
-﻿using GraphQL.Types;
+﻿using System;
+using GraphQL.Types;
 
 namespace DotNetGraphQL.API
 {
     public class ImagesSchema : Schema
     {
+        static readonly Lazy<ImagesQuery> _imagesQueryHolder = new Lazy<ImagesQuery>(new ImagesQuery());
+
         public ImagesSchema()
         {
-            Query = new ImagesQuery();
+            Query = _imagesQueryHolder.Value;
         }
     }
 }
