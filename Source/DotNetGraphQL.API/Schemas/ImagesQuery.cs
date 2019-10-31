9d7fa06e9a82e11d8346c00731650276052a76cf
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-10-30 17:02:41 -0700

    Removed GetDogImagesByNameOrBreed

--- Source/DotNetGraphQL.API/Schemas/ImagesQuery.cs
+++ Source/DotNetGraphQL.API/Schemas/ImagesQuery.cs
@@ -16,22 +16,5 @@ namespace DotNetGraphQL.API
                     new QueryArguments(new QueryArgument<NonNullGraphType<StringGraphType>> { Name = "name", Description = "Dog Name" }),
                     context => DogImagesData.DogImages.Single(x => x.Title.Equals(context.GetArgument<string>("name"), StringComparison.OrdinalIgnoreCase)));
         }
-
-        IEnumerable<DogImagesModel> GetDogImagesByNameOrBreed(string? coatColor, string? breed)
-        {
-            bool doesCoatColorContainValue = !string.IsNullOrWhiteSpace(coatColor);
-            bool doesBreedContainValue = !string.IsNullOrWhiteSpace(breed);
-
-            if (doesCoatColorContainValue && doesBreedContainValue)
-                return DogImagesData.DogImages.Where(x => x.CoatColor.Equals(coatColor, StringComparison.OrdinalIgnoreCase) && x.Breed.Equals(breed, StringComparison.OrdinalIgnoreCase));
-
-            if (doesCoatColorContainValue)
-                return DogImagesData.DogImages.Where(x => x.CoatColor.Equals(coatColor, StringComparison.OrdinalIgnoreCase));
-
-            if (doesBreedContainValue)
-                return DogImagesData.DogImages.Where(x => x.Breed.Equals(breed, StringComparison.OrdinalIgnoreCase));
-
-            throw new ArgumentNullException($"{nameof(DogImagesModel.CoatColor)} and {nameof(DogImagesModel.Breed)} cannot both be null");
-        }
     }
 }
