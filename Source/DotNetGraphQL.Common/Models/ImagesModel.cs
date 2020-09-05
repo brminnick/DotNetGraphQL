bb80dd89c9becfe71d3449dbc01a556bc6be4021
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-09-05 11:35:43 -0700

    Update NuGet Packages

--- Source/DotNetGraphQL.Common/Models/ImagesModel.cs
+++ Source/DotNetGraphQL.Common/Models/ImagesModel.cs
@@ -6,7 +6,7 @@ namespace DotNetGraphQL.Common
     public abstract class ImagesModel
     {
         protected ImagesModel(string avatarUrl, string websiteUrl, string title, IEnumerable<string> imagesList) =>
-            (AvatarUrl, WebsiteUrl, Title, ImagesList) = (avatarUrl, websiteUrl, title, imagesList.ToList());
+            (AvatarUrl, WebsiteUrl, Title, ImagesList) = (avatarUrl, websiteUrl, title, imagesList.ToArray());
 
         public string AvatarUrl { get; }
         public string WebsiteUrl { get; }
